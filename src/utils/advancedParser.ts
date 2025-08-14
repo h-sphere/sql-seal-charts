46ab0b037c2d4e514921a71971ced22f97ed897e
Kacper Kula <kulak@kulak.at>
2025-08-14 16:30:22 +0100

    fix: fixing issue with columns not being directly available in Advanced Mode

--- src/utils/advancedParser.ts
+++ src/utils/advancedParser.ts
@@ -1,24 +1,41 @@
-import { uniqBy } from "lodash"
+import { uniqBy } from "lodash";
+import { identifier } from "safe-identifier";
 
 type Params = {
-    functions: Record<string, any>,
-    variables: Record<string, any>
-    configs: Record<string, any>
-}
+  functions: Record<string, any>;
+  variables: Record<string, any>;
+  configs: Record<string, any>;
+};
 
-export const parseCodeAdvanced = ({ functions, variables, configs }: Params, code: string) => {
-    const { keys, values } = uniqBy([
-        ...Object.entries(functions),
-        ...Object.entries({
-            data: variables.data,
-            columns: variables.columns,
-            ...configs
-        })
-    ], 0).reduce(({ keys, values}, [key, value]) => ({
-        keys: [...keys, key],
-        values: [...values, value]
-    }), { keys: [], values: []})
-    const fn = new Function(...keys, code)
+const sanatiseColumns = (columns: Record<string, unknown>) => {
+  return Object.fromEntries(
+    Object.entries(columns).map(([key, value]) => [identifier(key), value])
+  );
+};
 
-    return fn(...values)
-}
+export const parseCodeAdvanced = (
+  { functions, variables, configs }: Params,
+  code: string
+) => {
+  const { keys, values } = uniqBy(
+    [
+      ...Object.entries(functions),
+      ...Object.entries({
+        ...sanatiseColumns(variables.columns),
+        data: variables.data,
+        columns: variables.columns,
+        ...configs,
+      }),
+    ],
+    0
+  ).reduce(
+    ({ keys, values }, [key, value]) => ({
+      keys: [...keys, key],
+      values: [...values, value],
+    }),
+    { keys: [], values: [] }
+  );
+  const fn = new Function(...keys, code);
+
+  return fn(...values);
+};
